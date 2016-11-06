f5e6972cfcdd2f219a0f820561863807bcc67c17
lrcannalongo <lrcannalongo@gmail.com>
2016-11-06 13:44:10 -0500

    Group design update 11/6/16

---
+++ AuctionObserver.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * Write a description of class AuctionObserver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AuctionObserver //implements Observer
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class AuctionObserver
+     */
+    public AuctionObserver()
+    {
+    }
+    
+    void update(Observable o, Object arg)
+    {}
+}
