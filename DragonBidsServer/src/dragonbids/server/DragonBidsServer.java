213f49c9bc1aadd714e7520e034c78d7174c5b28
Carmine Valentino <cvalentino3@gmail.com>
2016-11-20 18:57:18 -0500

    Removed syntax error in DragonBidsServer

--- DragonBidsServer/src/dragonbids/server/DragonBidsServer.java
+++ DragonBidsServer/src/dragonbids/server/DragonBidsServer.java
@@ -104,7 +104,6 @@ public class DragonBidsServer implements DragonBidsServer_I {
 		Vector<ListingSkeleton> listingSkeletonVector = new Vector<ListingSkeleton>();
 		
 		for (Listing listing : activeListings.values()) {
-			listing.
 		}
 		
 		return listingSkeletonVector;
